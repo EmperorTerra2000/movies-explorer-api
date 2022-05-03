f0286817e3f100cb61434f9c85a98bde9320ebd6
Abramov Trofim <EmperorTerra2000@yandex.ru>
2022-05-03 16:49:47 +0300

    fix: fixed attribut SameSite 2.0

--- controllers/signin.js
+++ controllers/signin.js
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       // создаем токен
       // первый аргумент это пейлоуд, т.е. что будет использоваться для создания хеша
-      let token = jwt.sign(
+      const token = jwt.sign(
         // пейлоуд
         { _id: user._id },
         // секретный ключ
