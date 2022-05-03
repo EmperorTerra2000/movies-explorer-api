c99e36638c613387e4f59c9503abd14cd2ecbfe7
Abramov Trofim <EmperorTerra2000@yandex.ru>
2022-05-03 18:24:29 +0300

    fix: fix signin 9.0

--- controllers/signin.js
+++ controllers/signin.js
@@ -38,7 +38,7 @@ module.exports.login = (req, res, next) => {
       res.header("Access-Control-Allow-Credentials", true);
 
       res.cookie("token", token, {
-        maxAge: 3600 * 168, // время жизни файла cookie в секундах ( 7 дней )
+        expires: new Date(Date.now() + 7 * 24 * 3600000), // время жизни файла cookie в секундах ( 7 дней )
         sameSite: "none",
         secure: true,
       });
